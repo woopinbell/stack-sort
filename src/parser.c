b2af64a9f462eb20f87674bbd3e517a5b68a74fe
seungwoo7050 <seungwoo7050@naver.com>
2025-02-09 15:28:42 +0900

    feat(parse): 공백으로 결합된 인자 토큰을 처리

--- src/parser.c
+++ src/parser.c
@@ -1,5 +1,46 @@
 #include "push_swap.h"
 
+static int	is_space(char c)
+{
+	return (c == ' ' || c == '\t' || c == '\n'
+		|| c == '\r' || c == '\v' || c == '\f');
+}
+
+static int	count_tokens_in_arg(const char *arg)
+{
+	int	count;
+	int	i;
+
+	count = 0;
+	i = 0;
+	while (arg[i] != '\0')
+	{
+		while (is_space(arg[i]))
+			i++;
+		if (arg[i] == '\0')
+			break ;
+		count++;
+		while (arg[i] != '\0' && !is_space(arg[i]))
+			i++;
+	}
+	return (count);
+}
+
+static int	count_all_tokens(int argc, char **argv)
+{
+	int	count;
+	int	i;
+
+	count = 0;
+	i = 1;
+	while (i < argc)
+	{
+		count += count_tokens_in_arg(argv[i]);
+		i++;
+	}
+	return (count);
+}
+
 static int	parse_token(const char *arg, int start, int end, int *out)
 {
 	long long	value;
@@ -34,30 +75,55 @@ static int	parse_token(const char *arg, int start, int end, int *out)
 	return (1);
 }
 
+static int	fill_values(int argc, char **argv, t_stack *a)
+{
+	int	i;
+	int	pos;
+	int	start;
+	int	end;
+	int	value;
+
+	i = 1;
+	pos = 0;
+	while (i < argc)
+	{
+		end = 0;
+		while (argv[i][end] != '\0')
+		{
+			while (is_space(argv[i][end]))
+				end++;
+			if (argv[i][end] == '\0')
+				break ;
+			start = end;
+			while (argv[i][end] != '\0' && !is_space(argv[i][end]))
+				end++;
+			if (!parse_token(argv[i], start, end, &value))
+				return (0);
+			a->values[pos] = value;
+			a->ranks[pos] = value;
+			pos++;
+		}
+		i++;
+	}
+	return (1);
+}
+
 int	parse_input(int argc, char **argv, t_stack *a)
 {
 	int	count;
-	int	index;
-	int	value;
 
 	stack_init_empty(a);
 	if (argc == 1)
 		return (1);
-	count = argc - 1;
+	count = count_all_tokens(argc, argv);
+	if (count == 0)
+		return (0);
 	if (!stack_init(a, count))
 		return (0);
-	index = 0;
-	while (index < count)
+	if (!fill_values(argc, argv, a))
 	{
-		if (!parse_token(argv[index + 1], 0,
-				(int)ps_strlen(argv[index + 1]), &value))
-		{
-			stack_free(a);
-			return (0);
-		}
-		a->values[index] = value;
-		a->ranks[index] = value;
-		index++;
+		stack_free(a);
+		return (0);
 	}
 	a->size = count;
 	return (1);
