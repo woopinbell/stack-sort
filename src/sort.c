b5966f0fa117d792260f3913068886c008c1e9ce
seungwoo7050 <seungwoo7050@naver.com>
2025-02-10 14:26:03 +0900

    feat(sort): 네다섯 개의 스택을 정렬

--- src/sort.c
+++ src/sort.c
@@ -1,5 +1,34 @@
 #include "push_swap.h"
 
+static int	find_rank_index(const t_stack *stack, int rank)
+{
+	int	i;
+
+	i = 0;
+	while (i < stack->size)
+	{
+		if (stack->ranks[i] == rank)
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
+static void	move_index_to_top(t_stack *a, int index)
+{
+	if (index <= a->size / 2)
+	{
+		while (index-- > 0)
+			op_ra(a, 1);
+	}
+	else
+	{
+		index = a->size - index;
+		while (index-- > 0)
+			op_rra(a, 1);
+	}
+}
+
 static void	sort_two(t_stack *a)
 {
 	if (a->ranks[0] > a->ranks[1])
@@ -35,13 +64,33 @@ static void	sort_three(t_stack *a)
 		op_rra(a, 1);
 }
 
+static void	sort_tiny(t_stack *a, t_stack *b)
+{
+	int	target_rank;
+	int	index;
+
+	if (a->size == 2)
+	{
+		sort_two(a);
+		return ;
+	}
+	target_rank = 0;
+	while (a->size > 3)
+	{
+		index = find_rank_index(a, target_rank);
+		move_index_to_top(a, index);
+		op_pb(a, b, 1);
+		target_rank++;
+	}
+	sort_three(a);
+	while (b->size > 0)
+		op_pa(a, b, 1);
+}
+
 void	sort_stack(t_stack *a, t_stack *b)
 {
-	(void)b;
 	if (a->size < 2 || stack_is_sorted(a))
 		return ;
-	if (a->size == 2)
-		sort_two(a);
-	else if (a->size == 3)
-		sort_three(a);
+	if (a->size <= 5)
+		sort_tiny(a, b);
 }
